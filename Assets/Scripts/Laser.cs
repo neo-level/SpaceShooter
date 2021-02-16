bdff8698b5288470917e3cc4e196d270e5965dbb
frederik desmet <info@neo-level.com>
2021-02-16 16:50:25 +0100

    Created a laser projectile prefab, intantiate on spacebar.

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // speed of 8.
+    [SerializeField] private float speed = 8.0f;   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timeBasedSpeed = speed * Time.deltaTime;
+        // translate the laser up.
+        transform.Translate(0, timeBasedSpeed, 0);
+        
+    }
+}
